32e6ef8d0b65c9cb1ada3f042316ff187a8b0d60
jakobniinja <dansaren124@outlook.com>
2024-04-22 11:54:18 +0200

    Add 4 jbutton with action listeners

--- src/main/java/com/jakobniinja/GuessMyColor.java
+++ src/main/java/com/jakobniinja/GuessMyColor.java
@@ -4,16 +4,36 @@ import static javax.swing.SwingConstants.CENTER;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.util.Random;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
+import javax.swing.WindowConstants;
 
 public class GuessMyColor extends JFrame {
 
   public static final Long SerialVersionUID = 1L;
+
+  private JPanel samplePanel = new JPanel();
+
+  private JPanel targetPanel = new JPanel();
+
+  private int targetRed = 0;
+
+  private int targetGreen = 0;
+
+  private int targetBlue = 0;
+
+  private int red = 0;
+
+  private int green = 0;
+
+  private int blue = 0;
 
   public GuessMyColor() {
     initGUI();
@@ -31,6 +51,9 @@ public class GuessMyColor extends JFrame {
     Font titleFont = new Font(Font.SERIF, Font.BOLD, 32);
     titleLabel.setFont(titleFont);
     titleLabel.setText("Guess My Color");
+    titleLabel.setBackground(Color.BLACK);
+    titleLabel.setForeground(Color.WHITE);
+    titleLabel.setOpaque(true);
     titleLabel.setHorizontalAlignment(CENTER);
     add(titleLabel, BorderLayout.PAGE_START);
     setTitle("Guess My Color");
@@ -38,6 +61,23 @@ public class GuessMyColor extends JFrame {
     JLabel centerLabel = new JLabel();
     centerLabel.setBackground(Color.BLUE);
     add(centerLabel, BorderLayout.CENTER);
+
+    JPanel centerPanel = new JPanel();
+    centerPanel.setBackground(Color.WHITE);
+    add(centerPanel, CENTER);
+
+    Dimension size = new Dimension(50, 50);
+    samplePanel.setBackground(Color.BLACK);
+    samplePanel.setPreferredSize(size);
+    centerPanel.add(samplePanel);
+
+    targetPanel.setBackground(Color.CYAN);
+    targetPanel.setPreferredSize(size);
+    centerPanel.add(targetPanel);
+
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.setBackground(Color.BLACK);
+    add(buttonPanel, BorderLayout.PAGE_END);
 
     JPanel leftPanel = new JPanel();
     leftPanel.setBackground(Color.RED);
@@ -50,7 +90,106 @@ public class GuessMyColor extends JFrame {
     JPanel bottomPanel = new JPanel();
     bottomPanel.setBackground(Color.YELLOW);
     add(bottomPanel, BorderLayout.PAGE_END);
-    //TODO: 2-8
+
+    Font font = new Font(Font.DIALOG, Font.BOLD, 18);
+
+    JButton moreRedButton = new JButton("+");
+    moreRedButton.setBackground(Color.YELLOW);
+    moreRedButton.setFont(font);
+    moreRedButton.addActionListener(e -> increaseRed());
+    buttonPanel.add(moreRedButton);
+
+    JButton lessRedButton = new JButton("-");
+    lessRedButton.setBackground(Color.RED);
+    lessRedButton.setFont(font);
+    lessRedButton.addActionListener(e -> decreaseRed());
+    buttonPanel.add(lessRedButton);
+
+    JButton moreGreenButton = new JButton("+");
+    moreGreenButton.setBackground(Color.GREEN);
+    moreGreenButton.setFont(font);
+    moreGreenButton.addActionListener(e -> increaseGreen());
+    buttonPanel.add(moreGreenButton);
+
+    JButton lessGreenButton = new JButton("-");
+    lessGreenButton.setBackground(Color.GREEN);
+    lessGreenButton.setFont(font);
+    lessGreenButton.addActionListener(e -> decreaseGreen());
+    buttonPanel.add(lessGreenButton);
+
+    JButton moreBlueButton = new JButton("+");
+    moreBlueButton.setBackground(Color.BLUE);
+    moreBlueButton.setFont(font);
+    moreBlueButton.addActionListener(e -> increaseBlue());
+    buttonPanel.add(moreBlueButton);
+
+    JButton lessBlueButton = new JButton("-");
+    lessBlueButton.setBackground(Color.BLUE);
+    lessBlueButton.setFont(font);
+    lessBlueButton.addActionListener(e -> decreaseBlue());
+    buttonPanel.add(lessBlueButton);
+
+    add(buttonPanel, BorderLayout.PAGE_END);
+
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    generateTargetColor();
+  }
+
+  private void increaseRed() {
+    if (red <= 240) {
+      red += 15;
+    }
+    updateColorSample();
+  }
+
+  private void decreaseRed() {
+    if (red >= 15) {
+      red -= 15;
+    }
+    updateColorSample();
+  }
+
+  private void increaseGreen() {
+    if (green <= 250) {
+      green += 15;
+    }
+    updateColorSample();
+  }
+
+  private void decreaseGreen() {
+    if (green >= 15) {
+      green -= 15;
+    }
+    updateColorSample();
+  }
+
+
+  private void increaseBlue() {
+    if (blue <= 250) {
+      blue += 15;
+    }
+    updateColorSample();
+  }
+
+  private void decreaseBlue() {
+    if (blue >= 15) {
+      blue -= 15;
+    }
+    updateColorSample();
+  }
+
+  private void generateTargetColor() {
+    Random random = new Random();
+    targetRed = random.nextInt(18) * 15;
+    targetGreen = random.nextInt(18) * 15;
+    targetBlue = random.nextInt(18) * 15;
+    Color targetColor = new Color(targetRed, targetGreen, targetBlue);
+    targetPanel.setBackground(targetColor);
+  }
+
+  public void updateColorSample() {
+    Color color = new Color(red, green, blue);
+    samplePanel.setBackground(color);
   }
 
   public static void main(String[] args) {
